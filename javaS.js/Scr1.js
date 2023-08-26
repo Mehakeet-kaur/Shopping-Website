7a23b10f5223929dbff2b553b3def7083967ad6a
Mehakeet-Kaur <mehakeet@gmail.com>
2023-08-26 19:48:57 +0530

    First Commit

--- javaS.js/Scr1.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector("#bar")
-const navmenu = document.querySelector("#navbar")
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active")
-    navmenu.classList.toggle("active")
-})
